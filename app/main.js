ea303243034e22075a79e2b14a44d5f40ebfac82
diiiiiego83 <dmoltenidev@gmail.com>
2016-08-15 23:48:36 -0500

    angular2 seed rc5

---
+++ app/main.js
@@ -0,0 +1,8 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app.module';
+// platformBrowserDynamic().bootstrapModule(AppModule);
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=main.js.map
